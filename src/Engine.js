fff2905dc75b63be989b91412836a183db693495
Jouin Alexis <alexis.jouin@live.fr>
2016-10-10 10:04:35 +0200

    GROS REFACTORING TO DO FIX : problème de placement de certaines billes (ex : black en c3 au lieu de a3) dans le test 8

--- src/Engine.js
+++ src/Engine.js
@@ -1,127 +1,126 @@
 "use strict";
+var Engine = function () {
 
-var plateau = new Array(6);
-var tmpPlateau = new Array(6);
-var i, j;
-var lineAdd, columnAdd = 0;
+    // private attributes and methods
+    var plateau = new Array(6), tmpPlateau = new Array(6);
+    var i, j, lineAdd, columnAdd = 0;
 
-for (i = 0; i < 6; i++) {
-    plateau[i] = new Array(6);
-    tmpPlateau[i] = new Array(6);
-    for (j = 0; j < 6; j++) {
-        plateau[i][j] = "vide";
-        tmpPlateau[i][j] = "vide";
-    }
-}
-
-var billeNumber;
-var turn = 1;
-var player;
-
-var charcodeA = parseInt("a".charCodeAt(0), 0);
-
-function videPlateau() {
-    billeNumber = 0;
-    return billeNumber;
-}
-
-function currentPlayer() {
-    if (turn % 2 === 1) {
-        player = "white";
-    } else {
-        player = "black";
-    }
-    return player;
-}
-
-function putBille(position) {
-    var line, column;
-    line = parseInt(position.charAt(1), 0) - 1;
-    column = parseInt(position.charCodeAt(0), 0) - charcodeA;
-
-    if (plateau[line][column] === "vide") {
-        plateau[line][column] = currentPlayer();
-        billeNumber++;
-        return true;
-    }
-    return false;
-}
-
-function billeOnPlateau() {
-    return billeNumber;
-}
-
-// retourne la couleur de la position
-function currentPlateau(position) {
-    var line, column;
-    line = parseInt(position.charAt(1), 0) - 1;
-    column = parseInt(position.charCodeAt(0), 0) - charcodeA;
-
-    /*
-     for (line ; line < plateau.length; line++) {
-     for (var column = 0; column < plateau.length; column++) {
-     if(plateau[line][column] !== null || plateau[line][column] !== "" || plateau[line][column] !== 0){*/
-    return plateau[line][column];
-    /*}
-     }
-
-     }*/
-}
-
-function replacePlateau() {
     for (i = 0; i < 6; i++) {
+        plateau[i] = new Array(6);
+        tmpPlateau[i] = new Array(6);
         for (j = 0; j < 6; j++) {
-            plateau[i][j] = tmpPlateau[i][j];
+            plateau[i][j] = "vide";
+            tmpPlateau[i][j] = "vide";
         }
     }
-}
+
+    var billeNumber, turn = 1, player, charcodeA = parseInt("a".charCodeAt(0), 0);
+
+    this.videPlateau = function () {
+        billeNumber = 0;
+        return billeNumber;
+    };
+
+    this.currentPlayer = function () {
+        if (turn % 2 === 1) {
+            player = "white";
+        } else {
+            player = "black";
+        }
+        return player;
+    };
+
+    this.putBille = function (position) {
+        var line, column;
+        line = parseInt(position.charAt(1), 0) - 1;
+        column = parseInt(position.charCodeAt(0), 0) - charcodeA;
+
+        if (plateau[line][column] === "vide") {
+            plateau[line][column] = this.currentPlayer();
+            billeNumber++;
+            return true;
+        }
+        return false;
+    };
+
+    this.billeOnPlateau = function () {
+        return billeNumber;
+    };
+
+// retourne la couleur de la position
+    this.currentPlateau = function (position) {
+        var line, column;
+        line = parseInt(position.charAt(1), 0) - 1;
+        column = parseInt(position.charCodeAt(0), 0) - charcodeA;
+
+        /*
+         for (line ; line < plateau.length; line++) {
+         for (var column = 0; column < plateau.length; column++) {
+         if(plateau[line][column] !== null || plateau[line][column] !== "" || plateau[line][column] !== 0){*/
+        return plateau[line][column];
+
+    };
+
+    this.replacePlateau = function () {
+        for (i = 0; i < 6; i++) {
+            for (j = 0; j < 6; j++) {
+                plateau[i][j] = tmpPlateau[i][j];
+            }
+        }
+    };
 
 
 //Rotation
-function rotate(l, c) {
-    lineAdd = l;
-    columnAdd = c;
-    tmpPlateau[lineAdd][2 + columnAdd] = plateau[lineAdd][columnAdd];
-    tmpPlateau[1 + lineAdd][2 + columnAdd] = plateau[lineAdd][1 + columnAdd];
-    tmpPlateau[2 + lineAdd][2 + columnAdd] = plateau[lineAdd][2 + columnAdd];
-    tmpPlateau[2 + lineAdd][1 + columnAdd] = plateau[1 + lineAdd][2 + columnAdd];//000000000000000000000
-    tmpPlateau[2 + lineAdd][columnAdd] = plateau[2 + lineAdd][2 + columnAdd];
-    tmpPlateau[1 + lineAdd][columnAdd] = plateau[2 + lineAdd][1 + columnAdd];
-    tmpPlateau[lineAdd][columnAdd] = plateau[2 + lineAdd][columnAdd];
-    tmpPlateau[lineAdd][1 + columnAdd] = plateau[1 + lineAdd][columnAdd];
-}
+    this.rotate = function (l, c) {
+        lineAdd = l;
+        columnAdd = c;
+        tmpPlateau[lineAdd][2 + columnAdd] = plateau[lineAdd][columnAdd];
+        tmpPlateau[1 + lineAdd][2 + columnAdd] = plateau[lineAdd][1 + columnAdd];
+        tmpPlateau[2 + lineAdd][2 + columnAdd] = plateau[lineAdd][2 + columnAdd];
+        tmpPlateau[2 + lineAdd][1 + columnAdd] = plateau[1 + lineAdd][2 + columnAdd];
+        tmpPlateau[2 + lineAdd][columnAdd] = plateau[2 + lineAdd][2 + columnAdd];
+        tmpPlateau[1 + lineAdd][columnAdd] = plateau[2 + lineAdd][1 + columnAdd];
+        tmpPlateau[lineAdd][columnAdd] = plateau[2 + lineAdd][columnAdd];
+        tmpPlateau[lineAdd][1 + columnAdd] = plateau[1 + lineAdd][columnAdd];
+    };
 
-function rotateInverse(l, c) {
-    lineAdd = l;
-    columnAdd = c;
-    tmpPlateau[lineAdd][columnAdd] = plateau[lineAdd][2 + columnAdd];
-    tmpPlateau[lineAdd][1 + columnAdd] = plateau[1 + lineAdd][2 + columnAdd];
-    tmpPlateau[lineAdd][2 + columnAdd] = plateau[2 + lineAdd][2 + columnAdd];
-    tmpPlateau[1 + lineAdd][2 + columnAdd] = plateau[2 + lineAdd][1 + columnAdd];
-    tmpPlateau[2 + lineAdd][2 + columnAdd] = plateau[2 + lineAdd][columnAdd];
-    tmpPlateau[2 + lineAdd][1 + columnAdd] = plateau[1 + lineAdd][columnAdd];
-    tmpPlateau[2 + lineAdd][columnAdd] = plateau[lineAdd][columnAdd];
-    tmpPlateau[1 + lineAdd][columnAdd] = plateau[lineAdd][1 + columnAdd];
-}
+    this.rotateInverse = function (l, c) {
+        lineAdd = l;
+        columnAdd = c;
+        tmpPlateau[lineAdd][columnAdd] = plateau[lineAdd][2 + columnAdd];
+        tmpPlateau[lineAdd][1 + columnAdd] = plateau[1 + lineAdd][2 + columnAdd];
+        tmpPlateau[lineAdd][2 + columnAdd] = plateau[2 + lineAdd][2 + columnAdd];
+        tmpPlateau[1 + lineAdd][2 + columnAdd] = plateau[2 + lineAdd][1 + columnAdd];
+        tmpPlateau[2 + lineAdd][2 + columnAdd] = plateau[2 + lineAdd][columnAdd];
+        tmpPlateau[2 + lineAdd][1 + columnAdd] = plateau[1 + lineAdd][columnAdd];
+        tmpPlateau[2 + lineAdd][columnAdd] = plateau[lineAdd][columnAdd];
+        tmpPlateau[1 + lineAdd][columnAdd] = plateau[lineAdd][1 + columnAdd];
+    };
 
-function rotateTopLeft(direction) {
-    if (direction === "clockwise") {
-        rotateInverse(0, 0);
-    } else {
-        rotate(0, 0);
-    }
-    replacePlateau();
-    turn++;
-    return true;
-}
+    this.rotateTopLeft = function (direction) {
+        if (direction === "clockwise") {
+            this.rotateInverse(0, 0);
+        } else {
+            this.rotate(0, 0);
+        }
+        this.replacePlateau();
+        turn += 1;
+        return true;
+    };
 
-function rotateTopRight(direction) {
-    if (direction === "clockwise") {
-        rotateInverse(0, 3);
-    } else {
-        rotate(0, 3);
-    }
-    replacePlateau();
-    turn++;
-    return true;
+    this.rotateTopRight = function (direction) {
+        if (direction === "clockwise") {
+            this.rotateInverse(0, 3);
+        } else {
+            this.rotate(0, 3);
+        }
+        this.replacePlateau();
+        turn += 1;
+        return true;
+    };
+
+
+    this.getPlateau = function () {
+        return plateau;
+    };
 }
--- test/test.js
+++ test/test.js
@@ -2,57 +2,63 @@
 
 var PalettoTestCase = TestCase("PalettoTestCase");
 
+
 PalettoTestCase = TestCase("TestCase");
 
+var e = new Engine();
+
+
 PalettoTestCase.prototype.test1 = function () {
-    assertTrue(videPlateau() === 0);
+    assertTrue(e.videPlateau() === 0);
 };
 
 PalettoTestCase.prototype.test2 = function () {
-    assertTrue(currentPlayer() === "white");
+    assertTrue(e.currentPlayer() === "white");
 };
 
 PalettoTestCase.prototype.test3 = function () {
-    assertTrue(putBille("a1") === true);
+    assertTrue(e.putBille("a1") === true);
 };
 
 PalettoTestCase.prototype.test4 = function () {
-    assertTrue(billeOnPlateau() === 1);
+    assertTrue(e.billeOnPlateau() === 1);
 };
 
 PalettoTestCase.prototype.test5 = function () {
-    assertTrue(rotateTopLeft("clockwise") === true);
+    assertTrue(e.rotateTopLeft("clockwise") === true);
 };
 
 PalettoTestCase.prototype.test6 = function () {
-    assertTrue(currentPlayer() === "black");// Joueur actuel est le Jnoir
+    assertTrue(e.currentPlayer() === "black");// Joueur actuel est le Jnoir
 };
 
 PalettoTestCase.prototype.test7 = function () {
-    assertTrue(putBille('a1') === true); //J NOIR place une bille en a1
-    assertTrue(billeOnPlateau() === 2); //Il doit y avoir 2 billes sur le plateau
+    assertTrue(e.putBille('a1') === true); //J NOIR place une bille en a1
+    assertTrue(e.billeOnPlateau() === 2); //Il doit y avoir 2 billes sur le plateau
 };
 
 PalettoTestCase.prototype.test8 = function () {
-    assertTrue(rotateTopLeft("anticlockwise") === true); //Rotation antiHorraire
-    assertTrue(billeOnPlateau() === 2); //Rotation antiHorraire
-    //assertTrue(currentPlateau("a1") === "white");
+    assertTrue(e.rotateTopLeft("anticlockwise") === true); //Rotation antiHorraire
+    assertTrue(e.billeOnPlateau() === 2); //Rotation antiHorraire
+    assertTrue(e.currentPlateau("a1") === "white");
 
-    console.log(currentPlateau("a1"));
-    console.log(currentPlateau("a3"));
-    console.log(currentPlateau("c1"));
+    console.log("a1 : " + e.currentPlateau("a1"));
+    console.log("a3 : " + e.currentPlateau("a3"));
+    console.log("c1 : " + e.currentPlateau("c1"));
 
-    console.log(plateau);
+    console.log(e.getPlateau());
 
 
 
 };
-
 
 
 PalettoTestCase.prototype.test9 = function () {
 
-    assertTrue(putBille("a1") === false); //On ne peut pas mettre en a1 car il y'a déjà une bille
-    assertTrue(currentPlayer() === "white"); //La main est toujours au joueur Blanc
+    assertTrue(e.putBille("a1") === false); //On ne peut pas mettre en a1 car il y'a déjà une bille
+    assertTrue(e.currentPlayer() === "white"); //La main est toujours au joueur Blanc
 };
+
+
+
 
